333784232743a87ca0dbbfb4b4466b763dce1bb7
natinha <naticamposs010607@gmail.com>
2023-11-28 06:10:28 -0300

    Relações
    Criei mais uma tabela (Atores) e aprendi a relacioná-la com a de Filmes por meio do JOIN.

--- run.py
+++ run.py
@@ -1,15 +1,18 @@
 from infra.repository.filmesRepository import FilmesRepository
+from infra.repository.atoresRepository import AtoresRepository
 
-repositorio = FilmesRepository() 
-
-'''Tá indo lá no repositório de filmes, que pega o DBConnection que está conecta ao banco'''
-
-#repositorio.insert("Verônica", "Suspense", 2022)
-
-#repositorio.delete("Verônica")
-
-repositorio.update("Batman", 2022)
-
+repositorio = AtoresRepository()
 data = repositorio.select()
-
 print(data)
+
+
+
+
+
+#repositorio = FilmesRepository() 
+'''Tá indo lá no repositório de filmes, que pega o DBConnection que está conecta ao banco'''
+#repositorio.insert("Verônica", "Suspense", 2022)
+#repositorio.delete("Verônica")
+#repositorio.update("Batman", 2022)
+#data = repositorio.select()
+#print(data)
